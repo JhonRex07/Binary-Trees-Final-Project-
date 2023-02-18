b74655d31a3f06930e74e128eacd49614c4b94c2
JhonRex07 <jhonrexjusayan07@gmail.com>
2023-02-18 13:50:29 +0800

    tkinter

---
+++ Binary Trees Final Project.py
@@ -0,0 +1,10 @@
+# PROJECT ON DATA STRUCTURES AND ALGORITHMS
+# Programmed by JHON REX B. JUSAYAN
+
+from tkinter import *
+
+main_window = Tk()
+main.window.title("Python Binary Tree")
+main.window.geometry("400x275")\
+
+mainloop
